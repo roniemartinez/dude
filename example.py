be133f56f443e4f3c4bd08ee2280f0b7a36b22f0
Ronie Martinez <ronmarti18@gmail.com>
2022-02-14 22:58:26 +0100

    ✨ Add Setup and Navigate actions

--- example.py
+++ example.py
@@ -28,5 +28,23 @@ def result_description(element):
     return {"description": element.text_content()}
 
 
+@dude.selector("text=I agree", setup=True)
+def agree(element, page):
+    """
+    Clicks "I agree" in order to use the website.
+    """
+    with page.expect_navigation():
+        element.click()
+
+
+@dude.selector("text=Next", navigate=True)
+def next_page(element, page):
+    """
+    Clicks the Next button/link to navigate to the next page.
+    """
+    with page.expect_navigation():
+        element.click()
+
+
 if __name__ == "__main__":
-    dude.run(url="https://www.google.com/search?q=dude&hl=en")
+    dude.run(url="https://www.google.com/search?q=dude&hl=en", pages=2)
