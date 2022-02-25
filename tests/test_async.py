4039486c8c606467bc7e980ddacf4ec9b423a38b
Ronie Martinez <ronmarti18@gmail.com>
2022-02-25 02:20:16 +0100

    Fix mypy errors

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,6 +1,6 @@
 import json
 import sys
-from typing import Dict, List
+from typing import Any, Dict, List
 from unittest import mock
 
 import httpx
@@ -38,7 +38,7 @@ def test_full_flow_bs4(
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="AsyncMock is not supported.")
 @mock.patch.object(httpx, "AsyncClient")
 def test_full_flow_bs4_httpx(
-    mock_client: mock.AsyncMock,
+    mock_client: Any,  # mock.AsyncMock
     scraper_application: Scraper,
     async_bs4_select: None,
     expected_data: List[Dict],
@@ -71,7 +71,7 @@ def test_select(
 def test_async_save(
     scraper_application: Scraper, async_playwright_select: None, expected_data: List[Dict], test_url: str
 ) -> None:
-    mock_save = mock.AsyncMock()
+    mock_save = mock.AsyncMock()  # type: ignore[attr-defined]
     mock_save.return_value = True
     scraper_application.save(format="custom")(mock_save)
     scraper_application.run(urls=[test_url], pages=2, format="custom", parser="playwright")
