4feba83960b771388d959a4fee19746c6584535b
Ronie Martinez <ronmarti18@gmail.com>
2022-03-04 02:01:35 +0100

    🐛 Fix incompatible types when sorting (#48)
    * 🐛 Fix incompatible types when sorting
    
    - Raise exception when BeautifulSoup4 is not installed
    
    * Fix lint

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -8,7 +8,7 @@ from dude.rule import Selector
 
 
 @pytest.mark.parametrize(
-    ("group", "selector_str", "selector_str_with_type"),
+    ("selector", "selector_str", "selector_str_with_type"),
     (
         (Selector(selector="test"), "test", "test"),
         (Selector(css="test"), "test", "css=test"),
@@ -17,15 +17,21 @@ from dude.rule import Selector
         (Selector(regex="test"), "test", "text=/test/i"),
     ),
 )
-def test_to_str(group: Selector, selector_str: str, selector_str_with_type: str) -> None:
-    assert group.to_str(with_type=False) == selector_str
-    assert group.to_str(with_type=True) == selector_str_with_type
+def test_to_str(selector: Selector, selector_str: str, selector_str_with_type: str) -> None:
+    assert selector.to_str(with_type=False) == selector_str
+    assert selector.to_str(with_type=True) == selector_str_with_type
 
 
 def test_invalid_to_str() -> None:
-    group = Selector()
+    selector = Selector()
     with pytest.raises(AssertionError):
-        group.to_str()
+        selector.to_str()
+
+
+def test_comparison() -> None:
+    selector1 = Selector(css="test")
+    selector2 = Selector(xpath="test")
+    assert selector1 < selector2
 
 
 def test_no_selector(scraper_application: Scraper) -> None:
