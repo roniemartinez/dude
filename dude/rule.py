e47098c6898bb5d47ec8b66272793fc8e773792f
Ronie Martinez <ronmarti18@gmail.com>
2022-03-04 01:52:12 +0100

    Fix lint

--- dude/rule.py
+++ dude/rule.py
@@ -27,7 +27,7 @@ class Selector(NamedTuple):
     def __str__(self) -> str:
         return self.selector or self.css or self.xpath or self.text or self.regex or ""
 
-    def __lt__(self, other: "Selector") -> bool:
+    def __lt__(self, other: "Selector") -> bool:  # type: ignore
         return self.to_str(with_type=True) < other.to_str(with_type=True)
 
 
